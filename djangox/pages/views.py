fcd917d3af62710980dfaa5f63e3718b2d0a6a5c
kenion <k.gurin@g.nsu.ru>
2023-05-12 10:43:31 +0700

    Imroved server

--- djangox/pages/views.py
+++ djangox/pages/views.py
@@ -1,4 +1,9 @@
+import time
+from django.http import HttpResponse, JsonResponse
 from django.views.generic import TemplateView
+from django.shortcuts import render
+from sympy import *
+import numpy as np
 
 class HomePageView(TemplateView):
     template_name = "pages/home.html"
@@ -6,3 +11,24 @@ class HomePageView(TemplateView):
 class AboutPageView(TemplateView):
     template_name = "pages/about.html"
 
+class PlotPageView(TemplateView):
+    template_name = "pages/plot.html"
+
+    def post(self, request, *args, **kwargs):
+        f = request.POST.get('function', '')
+        x_values = np.linspace(-1000000, 1000000, 100)
+        y_values = []
+        x = symbols('x')
+        expr = sympify(f)
+        f_np = lambdify(x, expr, "numpy")
+
+        for x_val in x_values:
+            time.sleep(0.01)
+            y_val = f_np(x_val)
+            y_values.append(y_val)
+
+        data = {"x_values": list(x_values), "y_values": list(y_values)}
+        return JsonResponse(data)
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name, {})
