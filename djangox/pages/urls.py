b98d4555641b761643a048ff593f09e298a3664d
kenion <k.gurin@g.nsu.ru>
2023-05-24 04:50:22 +0700

    More text pages added

--- djangox/pages/urls.py
+++ djangox/pages/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from .views import HomePageView, AboutPageView, PlotPageView, TextPageView
+from .views import HomePageView, AboutPageView, PlotPageView, TextPageView1, TextPageView2, TextPageView3, TextPageView4, TextPageView5
 
 urlpatterns = [
     path("", HomePageView.as_view(), name="home"),
     path("about/", AboutPageView.as_view(), name="about"),
     path("plot/", PlotPageView.as_view(), name="plot"),
-    path("text/", TextPageView.as_view(), name="text"),
+    path("text1/", TextPageView1.as_view(), name="text1"),
+    path("text2/", TextPageView2.as_view(), name="text2"),
+    path("text3/", TextPageView3.as_view(), name="text3"),
+    path("text4/", TextPageView4.as_view(), name="text4"),
+    path("text5/", TextPageView5.as_view(), name="text5"),
 ]
--- djangox/pages/views.py
+++ djangox/pages/views.py
@@ -36,8 +36,8 @@ class PlotPageView(TemplateView):
         return JsonResponse(data)
     
 
-class TextPageView(TemplateView):
-    template_name = "pages/text.html"
+class TextPageView1(TemplateView):
+    template_name = "pages/text/text1.html"
 
     def get(self, request, *args, **kwargs):
         time.sleep(2)
@@ -45,6 +45,58 @@ class TextPageView(TemplateView):
         html = response.content
         soup = BeautifulSoup(html, 'html.parser')
         pre_tag = soup.find('xxx7')
-        text = pre_tag.get_text()
-        context = {'text': text}
+        text1 = pre_tag.get_text()
+        context = {'text1': text1}
+        return render(request, self.template_name, context)
+    
+class TextPageView2(TemplateView):
+    template_name = "pages/text/text2.html"
+
+    def get(self, request, *args, **kwargs):
+        time.sleep(2)
+        response = requests.get('http://az.lib.ru/g/gonkur_z_e/text_1867_manette_salomon-oldorfo.shtml')
+        html = response.content
+        soup = BeautifulSoup(html, 'html.parser')
+        pre_tag = soup.find('xxx7')
+        text2 = pre_tag.get_text()
+        context = {'text2': text2}
+        return render(request, self.template_name, context)
+    
+class TextPageView3(TemplateView):
+    template_name = "pages/text/text3.html"
+
+    def get(self, request, *args, **kwargs):
+        time.sleep(2)
+        response = requests.get('http://az.lib.ru/g/gonkur_z_e/text_1877_la_fille_elisa-oldorfo.shtml')
+        html = response.content
+        soup = BeautifulSoup(html, 'html.parser')
+        pre_tag = soup.find('xxx7')
+        text3 = pre_tag.get_text()
+        context = {'text3': text3}
+        return render(request, self.template_name, context)
+    
+class TextPageView4(TemplateView):
+    template_name = "pages/text/text4.html"
+
+    def get(self, request, *args, **kwargs):
+        time.sleep(2)
+        response = requests.get('http://az.lib.ru/w/wega_l_d/text_1897_poslednyaya_lubov_lope_de_vega-oldorfo.shtml')
+        html = response.content
+        soup = BeautifulSoup(html, 'html.parser')
+        pre_tag = soup.find('xxx7')
+        text4 = pre_tag.get_text()
+        context = {'text4': text4}
+        return render(request, self.template_name, context)
+    
+class TextPageView5(TemplateView):
+    template_name = "pages/text/text5.html"
+
+    def get(self, request, *args, **kwargs):
+        time.sleep(2)
+        response = requests.get('http://az.lib.ru/m/mann_g/text_1907_zwischen_den_rassen-oldorfo.shtml')
+        html = response.content
+        soup = BeautifulSoup(html, 'html.parser')
+        pre_tag = soup.find('xxx7')
+        text5 = pre_tag.get_text()
+        context = {'text5': text5}
         return render(request, self.template_name, context)
