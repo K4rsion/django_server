256451ac127deff3304d7fbf847f47e5f7371136
Your Name <you@example.com>
2023-05-24 13:58:51 +0700

    Languages

--- djangox/django_project/middleware.py
+++ djangox/django_project/middleware.py
@@ -15,20 +15,10 @@ class MyMiddleware:
         if hasattr(request, 'path') and 'text' in request.path:
             response['Cache-Control'] = "public, max-age=600"
             return response
-
-        #для авторизованных пользователей
-        if request.user.is_authenticated:
-            if hasattr(request, 'path') and 'about' in request.path:
-                response['Cache-Control'] = "private, max-age=600"
-            elif hasattr(request, 'path') and '' in request.path:
-                response['Cache-Control'] = "private, no-transform, max-age=600"
+        if hasattr(request, 'path') and 'about' in request.path:
+            response['Cache-Control'] = "no-store,  no-cache"
             return response
-        
-        # для неавторизованных пользователей
-        elif request.user.is_anonymous:
-            if hasattr(request, 'path') and 'about' in request.path:
-                response['Cache-Control'] = "public, max-age=600"
-            elif hasattr(request, 'path') and '' in request.path:
-                response['Cache-Control'] = "public, no-transform, max-age=600"
+        if hasattr(request, 'path') and '' in request.path:
+            response['Cache-Control'] = "no-store,  no-cache"
             return response
         
