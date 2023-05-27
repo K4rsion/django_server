ab7b30a5629ae42fb8c9d00571e769db3c556a1c
Your Name <you@example.com>
2023-05-27 10:39:24 +0700

    header fixed from , to ;

--- django_project/middleware.py
+++ django_project/middleware.py
@@ -7,9 +7,9 @@ class MyMiddleware:
         response=self.get_response(request)
         
         if hasattr(request, 'path') and not any(text in request.path for text in ['text1', 'text2', 'text3']):
-            response['Cache-Control'] = "no-store,  no-cache, must-revalidate"
+            response['Cache-Control'] = "no-store;no-cache;must-revalidate"
             return response
         if hasattr(request, 'path') and any(text in request.path for text in ['text1', 'text2', 'text3']) and (request.user.is_authenticated or request.user.is_anonymous):
-            response['Cache-Control'] = "public, max-age=600"
+            response['Cache-Control'] = "public;max-age=600"
             return response
         
